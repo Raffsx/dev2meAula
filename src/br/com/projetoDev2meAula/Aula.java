ee1e93d95be45b1634f5cfb034b02a353c9082ac
Rafael de Agostinho <52942590+Raffsx@users.noreply.github.com>
2021-03-24 12:55:23 -0300

    Soma

--- src/br/com/projetoDev2meAula/Aula.java
+++ src/br/com/projetoDev2meAula/Aula.java
@@ -4,7 +4,12 @@ public class Aula {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world");
+		int n1 = 2;
+		int n2 = 2;
+		int soma = 0;
+		
+		soma = n1 + n2;
+		System.out.println(soma);
 	}
 
 }
