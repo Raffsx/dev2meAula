6a6d5a133b1b86026b8b3db5dd30531a7c2af040
Rafael de Agostinho <52942590+Raffsx@users.noreply.github.com>
2021-03-24 13:00:22 -0300

    Atividade 1 ok

--- src/br/com/projetoDev2meAula/Aula.java
+++ src/br/com/projetoDev2meAula/Aula.java
@@ -1,15 +1,25 @@
 package br.com.projetoDev2meAula;
 
+import javax.swing.JOptionPane;
+
 public class Aula {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int n1 = 2;
-		int n2 = 2;
+		
+		String nome = JOptionPane.showInputDialog("Digite seu nome");
+		String sobrenome = JOptionPane.showInputDialog("Digite seu sobrenome");
+		System.out.println(nome +" "+sobrenome);
+		
+		int n1 = Integer.valueOf(JOptionPane.showInputDialog("Digite seu numero"));;
+		int n2 = Integer.valueOf(JOptionPane.showInputDialog("Digite mais um numero"));;
+
 		int soma = 0;
 		
 		soma = n1 + n2;
 		System.out.println(soma);
+		
+		
 	}
 
 }
