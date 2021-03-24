1f39b76264e5c85b5c1842ff36dfd41da89ecf71
Rafael de Agostinho <52942590+Raffsx@users.noreply.github.com>
2021-03-24 12:52:17 -0300

    Hello World

---
+++ src/br/com/projetoDev2meAula/Aula.java
@@ -0,0 +1,10 @@
+package br.com.projetoDev2meAula;
+
+public class Aula {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+	}
+
+}
